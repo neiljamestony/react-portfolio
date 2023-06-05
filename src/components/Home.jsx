e5130a94cca84f34edb1861394a37e30137d9e7f
Perdigon <neil.james.perdigon@accenture.com>
2023-06-05 10:29:33 +0800

    added social media icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@ import About from './About';
 import Experience from './Experience';
 import Projects from './Projects';
 import Education from './Education';
+import LinkedIn from '../assets/img/linkedin-logo.png';
+import GithubIcon from '../assets/img/github-sign.png';
+import InstagramIcon from '../assets/img/instagram.png';
 
 export default function Home() {
   const handleDownload = () => {
@@ -41,6 +44,29 @@ export default function Home() {
             >
               DOWNLOAD CV
             </button>
+          </div>
+          <div className='social-media'>
+            <a
+              href='https://www.linkedin.com/in/neil-james-tony-perdigon-631422207'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src={LinkedIn} alt='linked-in' height={30} />
+            </a>
+            <a
+              href='https://github.com/neiljamestony'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src={GithubIcon} alt='github-icon' height={30} />
+            </a>
+            <a
+              href='https://instagram.com/nljmstny?igshid=MzNlNGNkZWQ4Mg=='
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src={InstagramIcon} alt='instagram-icon' height={30} />
+            </a>
           </div>
         </div>
       </div>
